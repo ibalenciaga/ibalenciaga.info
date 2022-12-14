75ea3e94c5808c6af813c12c73de7bba29626d22
ibalenciaga <ibalenciaga@gmail.com>
2022-12-14 11:07:06 +0100

    mail send test

--- sendmail.php
+++ sendmail.php
@@ -1,8 +1,8 @@
 <?php
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
-$from = "test@hostinger-tutorials.com";
-$to = "test@hostinger.com";
+$from = "ibalenciaga@gmail.com";
+$to = "ibalenciaga@gmail.com";
 $subject = "Checking PHP mail";
 $message = "PHP mail works just fine";
 $headers = "From:" . $from;
