3518ff144df6fe5501e4a60c17ad02bac6abe044
Iñaki Balenciaga <ibalenciaga@gmail.com>
2022-12-16 22:45:16 +0100

    Delete smtp-connection.php

--- helper/smtp-connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-Class smtpConection{
-    public string $host = "smtp.servidor-correo.net";
-    public string $username = "contacto@ibalenciaga.info";
-    public string $password = "18aL3N#C1aGa";
-}
