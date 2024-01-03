a97f09fae9934587e9381c1af7ea804b5a083da3
ibalenciaga <ibalenciaga@gmail.com>
2024-01-03 06:55:32 +0100

    update experience data

--- includes/datos.php
+++ includes/datos.php
@@ -1,10 +1,10 @@
 <?php
 $conocimientos = Array(
-    "html5", "css3", "javascript", "jquery", "php", "symfony", "vuejs", "java", "mysql", "docker", "wordpress", "prestashop", "joomla", "photoshop"
+    "html5", "css3", "javascript", "php", "api-rest", "symfony", "vuejs", "jquery", "mysql", "docker", "wordpress", "prestashop", "joomla", "photoshop"
 );
 
 $experiencias = array(
-    array("Desarrollador Web","BIBAGU","2022 - Actualidad", "Crear nuevas funcionalidades a la aplicación web de la empresa utilizando Codeigniter y Vue.js. Crear nuevos endpoint para la API con php y después hacer las llamadas en el front con javascript."),
+    array("Desarrollador Web","BIBAGU","2022 - 2023", "Crear nuevas funcionalidades a la aplicación web de la empresa utilizando Codeigniter y Vue.js. Crear nuevos endpoint para la API con php y después hacer las llamadas en el front con javascript."),
     array("Desarrollador Web","OARO","2021 - 2022", "Crear nuevas páginas para las webs de diferentes clientes utilizando Symfony, twig, css3 y javcascript."),
     array("Desarrollador Web","Lombok","2019 - 2020", "Crear nuevas páginas web para diferenetes clientes utilizando Wordpress, css3, javascript, jquery, php y mysql"),
     array("Desarrollador Web","Feel Free Rentals","2017 - 2018", "Crear nuevas funcionalidades a la aplicación web interna de la empresa con el framewrok Joomla. Crear nuevas páginas y funcionalidades a la web de la empresa utilizando el framwork Symfony, css3, javascript, jquery y ajax"),
@@ -12,6 +12,7 @@ $experiencias = array(
 );
 
 $cursos = array(
+    array("Symfony 7","2023"),
     array("PHP, SQL, POO, MVC, Laravel, Symfony, Wordpress","Septiembre 2022"),
     array("Arquitectura Software Moderna: DDD, Eventos, Microservicios","Julio 2022"),
     array("Principios SOLID con ejemplos","Julio 2022"),
@@ -33,7 +34,8 @@ $estudios = array(
 );
 
 $freelance = array(
-    array("Iparki","2018"),
-    array("Ilargi Berdea","2019 - 2020")
+    array("Peña Hñipica donostiarra", "2023 - Actualidad"),
+    array("Ilargi Berdea","2019 - 2020"),
+    array("Iparki","2018")
 );
 ?>
